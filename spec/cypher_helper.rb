031e56afe21e6877e000078366d8d900e7e11cda
LuzbellaBackup <luzbella@mailfence.com>
2024-04-24 16:53:20 -0400

    test: addition and substraction to letter ascii index

--- spec/cypher_helper.rb
+++ spec/cypher_helper.rb
@@ -3,30 +3,15 @@
 require './cypher'
 
 # wrap the number
-def wrapper(range1, range2, num1, shift)
-  num = num1 + shift
-  range = range2 - range1
-  total = range1 + ((num - range1) % range)
-  total.chr
-end
-
-# wrap the number
-def shifter(letter, number)
-  num1 = letter.ord
-  if (65..90).cover?(num1)
-    wrapper(65, 91, num1, number)
-  elsif (97..122).cover?(num1)
-    wrapper(97, 123, num1, number)
-  else
-    letter
+describe Cypher do
+  describe '#wrapper' do
+    it "addition number by 'n'" do
+      wrap = Cypher.new
+      expect(wrap.wrapper(65, 91, 65, 1).eql?(66))
+    end
+    it "substraction number by 'n'" do
+      wrap = Cypher.new
+      expect(wrap.wrapper(66, 91, 65, 1).eql?(65))
+    end
   end
 end
-
-# wrap the number
-def encoder(string, num)
-  string.split('').map do |item|
-    shifter(item, num)
-  end.join('')
-end
-
-p encoder('What a string!', 5)
